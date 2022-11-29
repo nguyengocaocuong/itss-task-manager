dc619a832cc17278abda8bd659936b1748ed1ca9
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2022-11-29 12:55:02 +0700

    done ui signin signout resetpass page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
+  document.title = '決勝'
   const account = localStorage.getItem('user') ? JSON.stringify(localStorage.getItem('user')) : undefined
   return account ? <Home /> : <Navigate to="/sign-in" />
 }
